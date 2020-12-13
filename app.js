584e600e9e3954aa3acb5b95b180b8b2b0e9510b
DaKheera47 <shaheergoodboy@gmail.com>
2020-12-13 15:53:43 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const textToChange = document.getElementsByClassName("changing-text");
+const staticText = document.getElementsByClassName("static-text");
+const projectsContainer = document.getElementsByClassName(
+    "projects-container"
+)[0];
+const totalOptions = [
+    "shaheer sarfaraz",
+    "a student",
+    "A developer",
+];
+
+let i = 0;
+
+function scroller() {
+    projectsContainer.scrollIntoView(true);
+}
+
+setInterval(() => {
+    if (totalOptions[i]) {
+        anime({
+            targets: ".changing-text",
+            opacity: 0,
+            duration: 200,
+            direction: "alternate",
+            changeComplete: () => {
+                textToChange[0].innerHTML = totalOptions[i];
+            },
+            easing: "easeOutCubic",
+        });
+
+        i++;
+
+        if (!totalOptions[i]) {
+            i = 0;
+        }
+    }
+}, 2000);
